7d0cc252ecdd660ddac9c421a82539cf2eeb7c64
arturo29y3 <87209261+arturo29y3@users.noreply.github.com>
2021-12-13 08:50:25 -0600

    Add files via upload

---
+++ storage/framework/views/51b442dc026277658af7909357b2e8c792d92198.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\kenne\Desktop\4B TIDSM\Aplicaciones Web Orientada a Servicios 4 B° TM\zoro_laravel\laravel\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
